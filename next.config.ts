b166164910e5443c809db38e0cc62317a1391796
Morad0099 <musahmorad01@gmail.com>
2026-08-17 00:21:20 +0000

    Fix assetPrefix for port 8087

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: 'standalone',
+  basePath: '',
+  assetPrefix: 'http://31.187.74.65:8087',
 };
 
 export default nextConfig;
